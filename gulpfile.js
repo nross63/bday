5d6787f80b23a30ca7e7c415a57cfe154d20a24b
Nate <nross63@gmail.com>
2014-04-18 00:28:02 -0400

    fix style

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    imageop = require('gulp-image-optimization');
 
 gulp.task('default', function () {
+
     gulp.src('scss/**/*.scss')
         .pipe(watch(function(files) {
             return files.pipe(sass())
