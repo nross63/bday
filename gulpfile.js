bb9fdb65b96742178bb56a675834a673ae22e9b3
Nate <nross63@gmail.com>
2014-04-20 01:35:08 -0400

    configure gulpfile:

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,63 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     watch = require('gulp-watch'),
-    imageop = require('gulp-image-optimization');
-
-gulp.task('default', function () {
-
-    gulp.src('scss/**/*.scss')
-        .pipe(watch(function(files) {
-            return files.pipe(sass())
-                .pipe(gulp.dest('./css'));
-        }));
+    imageop = require('gulp-image-optimization'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    prefix = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    prefix = require('gulp-autoprefixer');
+// Autoprefix SCSS/CSS targeting last 2 verstions, browsers > 5%, ie 9, ie 8
+gulp.task('prefix', function(){
+	gulp.src('./css/*.css')
+  		.pipe(prefix(["last 2 version", "> 5%", "ie 9", "ie 8"], { cascade: true }))
+  		.pipe(gulp.dest('./css/'));
 });
+
+// Compile SASS to CSS JS
+gulp.task('sass', function () {
+    gulp.src('scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+});
+
+// Lint JS
+gulp.task('lint', function() {
+  return gulp.src('src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Concat & Minify JS
+gulp.task('minifyJS', function(){
+  return gulp.src('src/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(rename('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'));
+});
+
+// Concat & Minify CSS
+gulp.task('minifyCSS', function(){
+  return gulp.src('./css/*.css')
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(rename('all.min.css'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/css/'));
+});
+
+// Watch JS
+gulp.task('watchJS', function() {
+  gulp.watch('src/*.js', ['lint', 'minifyJS']);
+});
+
+// Watch CSS
+gulp.task('watchSCSS', function() {
+  gulp.watch('scss/**/*.scss', ['sass', 'prefix','minifyCSS']);
+});
+
+// Default
+gulp.task('default', ['watchSCSS', 'watchJS']);
